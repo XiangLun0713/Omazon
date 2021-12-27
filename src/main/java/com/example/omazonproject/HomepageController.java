035a73d0a854c3307472f587808650bc855892ba
Yu Xuan <u2102858@siswa.um.edu.my>
2021-12-27 21:02:05 +0800

    Delete autocomplete search 1.Delete autocomplete search 2.Change xmlns in all fxml from "http://javafx.com/javafx/11.0.0" back to "http://javafx.com/javafx/17"

--- src/main/java/com/example/omazonproject/HomepageController.java
+++ src/main/java/com/example/omazonproject/HomepageController.java
@@ -10,17 +10,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.stage.Stage;
 
-
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.Objects;
 
 
